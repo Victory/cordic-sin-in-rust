d2c65ed031cd2a09bf6c9c0ef7917b0d7109c50d
Victory <git@dfhu.org>
2014-10-26 14:57:34 -0400

    shaking lookup table

--- cordic-sin/src/main.rs
+++ cordic-sin/src/main.rs
@@ -1,3 +1,24 @@
+fn generate_table () {
+    let pi: f32 = 3.1415926536897932384626;
+    let k1: f32 = 0.6072529350088812561694; // 1/k
+    let num_bits: uint = 32;
+    let num_elms: uint = num_bits;
+    let mul: uint = 1<<(num_bits-2);
+
+    println!("Cordic sin in rust");
+    println!("num bits {}", num_bits);
+    println!("pi is {}", pi);
+    println!("k1 is {}", k1);
+
+    let shift: f32 = 2.0;
+    for ii in range(0, num_bits) {
+        let ipow: f32 = 1.0/shift.powi(ii as i32);
+        let cur: f32 = ipow.atan();
+        println!("table values {}", cur);
+    }
+}
+
+
 fn main() {
-    println!("Cordic sin in rust")
+    generate_table();
 }
