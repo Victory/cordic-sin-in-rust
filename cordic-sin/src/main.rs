737d80f257dd5db75117602f56b40dbe566a43d8
Victory <git@dfhu.org>
2014-10-27 19:34:55 -0400

    switch to f64

--- cordic-sin/src/main.rs
+++ cordic-sin/src/main.rs
@@ -1,22 +1,23 @@
 fn generate_table () {
-    let pi: f32 = 3.1415926536897932384626;
-    let k1: f32 = 0.6072529350088812561694; // 1/k
+    let pi: f64 = 3.1415926536897932384626;
+    let k1: f64 = 0.6072529350088812561694; // 1/k
     let num_bits: uint = 32;
-    let num_elms: uint = num_bits;
-    let mul: f32 = 1073741824.000000; // 1<<(bits-2)
+    //let num_elms: uint = num_bits;
+    let mul: uint = 1<<(num_bits-2);
 
     println!("Cordic sin in rust");
     println!("num bits {}", num_bits);
     println!("mul {}", mul);
+    println!("1<<(num_bits-2) {}", ((1i << (num_bits-2)) as f64));
     println!("pi is {}", pi);
     println!("k1 is {}", k1);
 
-    let shift: f32 = 2.0;
+    let shift: f64 = 2.0;
     for ii in range(0, num_bits) {
-        let ipow: f32 = 1f32/shift.powi(ii as i32);
-        let cur: f32 = ipow.atan() * mul;
+        let ipow: f64 = 1f64/shift.powi(ii as i32);
+        let cur: f64 = ipow.atan() * (mul as f64);
         //println!("table values {:.10f}", cur);
-        println!("table values 0x{}", std::f32::to_str_hex(cur));
+        println!("table values 0x{}", std::f64::to_str_hex(cur));
     }
 }
 
