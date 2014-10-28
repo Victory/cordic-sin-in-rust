7554a3abef5b5d0f8b6e3ec6d5114efd2e20f56b
Victory <git@dfhu.org>
2014-10-28 18:59:56 -0400

    print the table in  a way rust can compile

--- cordic-sin/src/main.rs
+++ cordic-sin/src/main.rs
@@ -12,15 +12,53 @@ fn generate_table () {
     println!("pi is {}", pi);
     println!("k1 is {}", k1);
 
+    println!("let cordic_tab = [");
+
     let shift: f64 = 2.0;
     for ii in range(0, num_bits) {
         let ipow: f64 = 1f64/shift.powi(ii as i32);
         let cur: f64 = ipow.atan() * (mul as f64);
-        //println!("table values {:.10f}", cur);
-        println!("table values 0x{}", std::f64::to_str_hex(cur));
+
+        if ii % 4 == 0 && ii > 0 {
+            println!("");
+        }
+        if ii == 0 {
+            print!("{:0.8f}f64, ", cur);
+        } else {
+            print!("{:0.8f}, ", cur);
+        }
     }
+
+    println!("];");
 }
 
+fn sin(theta: f64) {
+
+    let num_bits: uint = 32;
+    let mut d: int;
+    let z: int = theta as int;
+    let cordic_tab = [
+        843314856.53262615f64, 497837829.38176435, 263043836.58692065, 133525158.66814968, 
+        67021686.89696868, 33543515.72887244, 16775850.86663180, 8388437.33958306, 
+        4194282.66686198, 2097149.33333944, 1048575.66666686, 524287.95833334, 
+        262143.99479167, 131071.99934896, 65535.99991862, 32767.99998983, 
+        16383.99999873, 8191.99999984, 4095.99999998, 2048.00000000, 
+        1024.00000000, 512.00000000, 256.00000000, 128.00000000, 
+        64.00000000, 32.00000000, 16.00000000, 8.00000000, 
+        4.00000000, 2.00000000, 1.00000000, 0.50000000];
+
+    for ii in range(0, num_bits) {
+        if z >= 0 {
+            d = 0;
+        } else {
+            d = -1;
+        }
+        println!("{}", d);
+    }
+
+}
 fn main() {
     generate_table();
+
+    sin(0.5);
 }
