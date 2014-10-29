7a12c68c4aade56c9d244ba04286a52f7cea80fb
Victory <git@dfhu.org>
2014-10-29 19:10:40 -0400

    transmute f64 <--> int

--- cordic-sin/src/main.rs
+++ cordic-sin/src/main.rs
@@ -32,22 +32,39 @@ fn generate_table () {
     println!("];");
 }
 
+fn int_to_f64 (lhs: int) -> f64 {
+    let rhs: f64 = unsafe {
+        std::mem::transmute(lhs)
+    };
+    return rhs;
+}
+
+
+fn f64_to_int (lhs: f64) -> int {
+    let rhs: int = unsafe {
+        std::mem::transmute(lhs)
+    };
+    return rhs;
+}
+
+
 fn sin(theta: f64) {
 
     let num_bits: uint = 32;
 
     let k1: f64 = 0.6072529350088812561694; // 1/k
     let mul: uint = 1<<(num_bits-2);
-    let mut x: f64 = (mul as f64) * k1;
-    let mut y: f64 = 0.0;
-    let mut z: f64 = theta;
-    
-
+    let mut x: int = ((mul as f64) * k1) as int;
+    let mut y: int = 0;
+    let mut z: int = theta as int;
+    let mut tx: int;
+    let mut ty: int ;
+    let mut tz: int;
 
     let mut d: int;
 
-    
-    let z: int = theta as int;
+    println!("x {}", x);
+
     let cordic_tab = [
         843314856.53262615f64, 497837829.38176435, 263043836.58692065, 133525158.66814968, 
         67021686.89696868, 33543515.72887244, 16775850.86663180, 8388437.33958306, 
@@ -58,26 +75,28 @@ fn sin(theta: f64) {
         64.00000000, 32.00000000, 16.00000000, 8.00000000, 
         4.00000000, 2.00000000, 1.00000000, 0.50000000];
 
-    for ii in range(0, num_bits) {
+    for k in range(0, num_bits) {
         if z >= 0 {
             d = 0;
         } else {
             d = -1;
         }
-
-        /*
         tx = x - (((y>>k) ^ d) - d);
         ty = y + (((x>>k) ^ d) - d);
-        tz = z - ((cordic_ctab[k] ^ d) - d);
-        x = tx; y = ty; z = tz;
-        */
-
-        println!("{}", d);
+        tz = z - (( (cordic_tab[k] as int) ^ d) - d);
+        x = tx; 
+        y = ty; 
+        z = tz;
     }
-
 }
-fn main() {
-    generate_table();
 
+
+fn main() {
+    let i: int = 42882;
+    let f: f64 = 3498043.4824;
+    assert!(int_to_f64(f64_to_int(f)) == f);
+    assert!(f64_to_int(int_to_f64(i)) == i);
+
+    generate_table();
     sin(0.5);
 }
